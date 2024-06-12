2c6263b8838601b93f7a42b53305d27907f4ae95
prashant-80 <prashantsingh200502@gmail.com>
2024-06-12 18:12:08 +0530

    add-rate-limit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const rateLimit  = require('express-rate-limit');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
 const app = express();
 
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // 2 minutes
+	limit: 3, // Limit each IP to 3 requests per `window` (here, per 2 minutes).
+	});
+
 app.use(express.json());   //help to parse the incoming request body 
 app.use(express.urlencoded({extended:true}));  
 
+app.use(limiter)
 
 app.use('/api', apiRoutes);
 
